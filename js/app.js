c428907664436159193cdcc8de904aafc000f773
User Name <kauth333@gmail.com>
2022-12-08 23:01:25 -0500

    create Reset button and able to click on list to erase list

--- js/app.js
+++ js/app.js
@@ -1,11 +1,23 @@
 // CACHE ELEMENT REFRENCES
 const inputElement = document.querySelector("#task")
-const btnElement = document.querySelector("#submit-button")
+const addBtnElement = document.getElementById("submit-button")
 const ulElement = document.querySelector("#todo-list")
+const resetBtnElement = document.getElementById("reset-button")
 
-btnElement.addEventListener('click',function(evt){
+
+
+addBtnElement.addEventListener('click',function(evt){
     const li = document.createElement("li")
     li.textContent = inputElement.value
     inputElement.value = ''
     ulElement.appendChild(li)
+})
+
+resetBtnElement.addEventListener('click', function(evt){
+    ulElement.textContent = ''
+})
+
+
+ulElement.addEventListener('click', function(evt){
+   ulElement.textContent = ""
 })
